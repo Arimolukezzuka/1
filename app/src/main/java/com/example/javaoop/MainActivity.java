f96790bfa2b51483a0f2919a90edcd22bccbc11e
student_ait409 <prostoi.cchelovek@gmail.com>
2023-10-11 13:54:47 +0300

    анонимные классы

--- app/src/main/java/com/example/javaoop/MainActivity.java
+++ app/src/main/java/com/example/javaoop/MainActivity.java
@@ -4,14 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.ArrayList;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-       Cat cat1 = new Cat();
-       cat1.catchMouse(1);
-       cat1.catchMouse(3);
+       Button button = findViewById(R.id.button);
+       button.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v){
+
+         }
+       });
+       Cat singingCat = new Cat(){
+           @Override
+           public void talk(){
+               Log.i("talk()","I'm singing! la-la-la!" );
+           }
+       };
+       singingCat.talk();
+
+       Cat cat = new Cat();
+       cat.talk();
    }
 }
